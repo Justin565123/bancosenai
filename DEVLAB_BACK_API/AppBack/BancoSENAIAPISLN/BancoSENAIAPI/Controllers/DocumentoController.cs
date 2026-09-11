b65c7cce394b1c574fee1cdeb3b9bea3687e8df0
Justin565123 <pablo.l.batista6@aluno.senai.br>
2026-09-11 08:58:53 -0300

    feat(documento): validacao arquivo

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -19,6 +19,10 @@ namespace BancoSENAIAPI.Controllers
         [HttpPost("upload/{codigoCliente}")]
         public async Task<IActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
         {
+            if (arquivo == null | | arquivo.Length == 0)
+            {
+                return BadRequest("Nenhm arquivo foi enviado")
+            }
 
         }
     }
